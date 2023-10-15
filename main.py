3fc37511635f2c745ee4ae944bb126a0cd058faf
Pesho <tsvetin.panov@gmail.com>
2023-10-15 21:41:27 +0300

    Finnish the weather app

--- main.py
+++ main.py
@@ -10,9 +10,22 @@ days = st.slider("Forecast days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
+if place:
+    # Get the temp
+    try:
+        filter_data = get_data(place, days)
 
-d, t = get_data(place, days, option)
+        if option == "Temperature":
+            temperature = [dict["main"]["temp"] / 10 for dict in filter_data]
 
-temperatures = 1
-figure = px.line(x=1, y=2, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart()
+            days =[dict["dt_txt"] for dict in filter_data]
+            figure = px.line(x=days, y=temperature, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
+        elif option == "Sky":
+            images = {"Clear": "images/clean.png", "Clouds":"images/cloud.png",
+                      "Rain":"images/rain.png", "Snow":"images/snow.png"}
+            sky_condition = [dict["weather"][0]["main"] for dict in filter_data]
+            image_paths = [images[condition] for condition in sky_condition]
+            st.image(image_paths, width=150)
+    except KeyError:
+        st.write("That place don't exist")
